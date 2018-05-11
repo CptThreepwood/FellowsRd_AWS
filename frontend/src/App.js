8730c2e1af6b5eb22bd66a982de0bc68c7f71e90
David Jennens <d.jennens@gmail.com>
2018-05-12 01:53:41 +1000

    Settled on react-slick for carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
+import AccountCircle from 'material-ui/svg-icons/action/account-circle';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import BookingCalendar from './Calendar.js'
+import SplashSlides from './SplashSlides.js'
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const floating_style = {
+  margin: 12,
+  zIndex: 1,
+  position: 'fixed',
+  marginTop: '70vh',
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div class="calendarContainer">
-          <BookingCalendar />
-        </div>
+        <MuiThemeProvider>
+          <RaisedButton
+            label="Sign In"
+            primary={true}
+            icon={<AccountCircle/>}
+            style={floating_style}
+          />
+          <div class="splashSlides">
+            <SplashSlides/>
+          </div>
+          {/* <div class="calendarContainer">
+            <BookingCalendar />
+          </div> */}
+        </MuiThemeProvider>
       </div>
     );
   }
