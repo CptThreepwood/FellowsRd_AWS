c695785be32dfa2ef0a90ada6dba2aa87d355a1f
David Jennens <d.jennens@gmail.com>
2018-06-04 21:40:53 +1000

    Logging in gets you to the booking calendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      authDetails: null,
+    }
+
+    this.updateAuth = this.updateAuth.bind(this);
+  }
+
+  updateAuth(authDetails) {
+    console.log(authDetails);
+    this.setState({'authDetails': authDetails});
+    this.setState({'isLoggedIn': true})
+  }
+
   render() {
-    return (
-      <div className="App">
-          <SplashSlides/>
-        {/* <div class="calendarContainer">
-          <BookingCalendar />
-        </div> */}
-      </div>
-    );
+    if (this.state.isLoggedIn) {
+      return (
+        <div className="App">
+          <div class="calendarContainer">
+            <BookingCalendar
+              authDetails={this.authDetails} />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+            <SplashSlides
+              updateAuth={this.updateAuth} />
+        </div>
+      );
+    }
   }
 }
 
