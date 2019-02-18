54c8d682749ce9563f00bb359170655cbb7a2787
David Jennens <d.jennens@gmail.com>
2019-02-19 01:07:00 +1100

    One method for each lambda

--- backend/api_gateway.tf
+++ backend/api_gateway.tf
@@ -9,14 +9,31 @@ resource "aws_api_gateway_resource" "proxy_resource" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "proxy_method" {
+resource "aws_api_gateway_method" "get_getBookings" {
   rest_api_id   = "${aws_api_gateway_rest_api.booking_gateway.id}"
   resource_id   = "${aws_api_gateway_resource.proxy_resource.id}"
-  http_method   = "ANY"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "booking" {
+resource "aws_api_gateway_integration" "getBookings" {
+  rest_api_id = "${aws_api_gateway_rest_api.booking_gateway.id}"
+  resource_id = "${aws_api_gateway_method.proxy_method.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_method.http_method}"
+
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.createBooking.invoke_arn}"
+}
+
+resource "aws_api_gateway_method" "post_createBooking" {
+  rest_api_id   = "${aws_api_gateway_rest_api.booking_gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy_resource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "createBooking" {
   rest_api_id = "${aws_api_gateway_rest_api.booking_gateway.id}"
   resource_id = "${aws_api_gateway_method.proxy_method.resource_id}"
   http_method = "${aws_api_gateway_method.proxy_method.http_method}"
