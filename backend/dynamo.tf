16e7e7d7fff0bc9e05f6b43b8c58550017beea4d
David Jennens <d.jennens@gmail.com>
2019-03-05 01:28:18 +1100

    Each booking is unique in user and date

--- backend/dynamo.tf
+++ backend/dynamo.tf
@@ -2,11 +2,11 @@ resource aws_dynamodb_table booking_table {
     name            = "${var.app_name}_bookings"
     billing_mode    = "PAY_PER_REQUEST"
 
-    hash_key        = "bookingID"
+    hash_key        = "userID"
     range_key       = "startDate"
 
     attribute = {
-        name = "bookingID"
+        name = "userID"
         type = "S"
     }
     attribute = {
